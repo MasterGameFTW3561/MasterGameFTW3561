aa095b07f40a9febeb9a8ddd28e6860aad905a75
MasterGameFTW3561 <mastergame3561@gmail.com>
2017-10-24 20:41:51 +0800

    aaaa

---
+++ repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "mastergame3561@gmail.com",
+	"id": "Halloween",
+	"patches": {},
+	"servers": [
+		"https://github.com/MasterGameFTW3561/MasterGameFTW3561"
+	],
+	"title": "Akiteru98 the designer and me the modder"
+}
