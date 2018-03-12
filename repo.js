103c7ff420071b5eee532b0d7c34b01f5e4cf41b
MasterGameFTW3561 <30772354+MasterGameFTW3561@users.noreply.github.com>
2018-03-12 17:56:18 +0800

    added

--- repo.js
+++ repo.js
@@ -2,10 +2,10 @@
 	"contact": "mastergame3561@gmail.com",
 	"id": "MasterGameFTW3561",
 	"patches": {
+		"Fr00sk_Styled": "Fr00sk Styled by MasterGameFTW3561 and Art by Fr00sk",
 		"Hatless": "Hatless Edits by MasterGameFTW3561 (WIP)",
 		"JzBoy_Styled": "JzBoy Styled by MasterGameFTW3561 and Art by JzBoy Anims",
-		"Koichi_Pose": "Koichi_Pose from JJBA by MasterGameFTW3561 and Art by Fr00sk",
-		"Fr00sk_Styled": "Fr00sk Styled by MasterGameFTW3561 and Art by Fr00sk"
+		"Koichi_Pose": "Koichi_Pose from JJBA by MasterGameFTW3561 and Art by Fr00sk"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/MasterGameFTW3561/",
