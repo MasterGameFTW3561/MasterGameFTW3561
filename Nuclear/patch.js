d5c94f1995b9d2cb1f323cee92a123fbfef9ebed
MasterGameFTW3561 <30772354+MasterGameFTW3561@users.noreply.github.com>
2018-05-12 11:54:37 +0800

    half of fr00sk and nuke mod

---
+++ Nuclear/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "Nuclear",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/MasterGameFTW3561/Nuclear/",
+		"http://mirrors.thpatch.net/MasterGameFTW3561/Nuclear/"
+	],
+	"title": "Change the Unyu into Unyuke (Touhou 11 only)"
+}
--- repo.js
+++ repo.js
@@ -5,6 +5,7 @@
 		"Fr00sk_Styled": "Fr00sk Styled by MasterGameFTW3561 and Art by Fr00sk",
 		"JzBoy_Styled": "JzBoy Styled by MasterGameFTW3561 and Art by JzBoy Anims",
 		"Koichi_Pose": "Koichi_Pose from JJBA by MasterGameFTW3561 and Art by Fr00sk"
+		"Nuclear": "Change the Unyu into Unyuke (Touhou 11 only)"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/MasterGameFTW3561/",
