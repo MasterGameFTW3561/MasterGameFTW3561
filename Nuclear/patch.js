79697e426689f4c56e35030ca30286a610ba79f2
MasterGameFTW3561 <30772354+MasterGameFTW3561@users.noreply.github.com>
2018-05-12 12:00:36 +0800

    h

--- Nuclear/patch.js
+++ Nuclear/patch.js
@@ -7,5 +7,5 @@
 		"http://thcrap.nmlgc.net/repos/MasterGameFTW3561/Nuclear/",
 		"http://mirrors.thpatch.net/MasterGameFTW3561/Nuclear/"
 	],
-	"title": "Change the Unyu into Unyuke (Touhou 11 only)"
+	"title": "Change the Unyu into Unyuke Touhou 11 only"
 }
