3a83ffd69ce72745e36dc970caf560336b0850d0
MasterGameFTW3561 <30772354+MasterGameFTW3561@users.noreply.github.com>
2018-08-06 18:24:21 +0800

    added

--- repo.js
+++ repo.js
@@ -5,7 +5,8 @@
 		"Fr00sk_Styled": "Fr00sk Styled by MasterGameFTW3561 and Art by Fr00sk",
 		"JzBoy_Styled": "JzBoy Styled by MasterGameFTW3561 and Art by JzBoy Anims",
 		"Koichi_Pose": "Koichi_Pose from JJBA by MasterGameFTW3561 and Art by Fr00sk",
-		"Nuclear": "Change the Unyu into Unyuke Touhou 11 only"
+		"Nuclear": "Change the Unyu into Unyuke Touhou 11 only",
+		"YukariSans": "Change Yukari just like Sands"
 	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/MasterGameFTW3561/",
