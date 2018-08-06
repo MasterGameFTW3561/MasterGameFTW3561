42d04f0d016ef72eafb14f91722365037c30446f
MasterGameFTW3561 <30772354+MasterGameFTW3561@users.noreply.github.com>
2018-08-06 18:23:16 +0800

    new mod

---
+++ YukariSans/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "YukariSans",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/MasterGameFTW3561/YukariSans/",
+		"http://mirrors.thpatch.net/MasterGameFTW3561/YukariSans/"
+	],
+	"title": "Yukari become just like Sands."
+}
